dd274420dc3ea0b2f98239ded9ca1665106aaa5d
Mohit <arceus93007@gmail.com>
2026-04-05 22:08:10 +0530

    fixed the calc code

--- lecture4-Parsingreq/calc/sum.js
+++ lecture4-Parsingreq/calc/sum.js
@@ -6,21 +6,25 @@ const sumRequestHandeler=(req,res)=>{
     body.push(chunk);
   })
   var sum;
-  req.on('end',chunk=>{
+  req.on('end',()=>{
     const fullbody=Buffer.concat(body).toString();
     console.log(fullbody);
     const params=new URLSearchParams(fullbody);
     const bodyobject={};
     for(const[key,value] of params.entries()){
       bodyobject[key]=value;
+    }
 
       console.log(bodyobject);
       sum=Number(bodyobject.num1)+Number(bodyobject.num2);
-    }
+      console.log(sum);
+      res.write(`<h1>The sum of num1 and num2 is ${sum}</h1>`)
+    
+    
   })
   
-  res.write(`<h1>The sum of num1 and num2 is ${sum}</h1>`)
-  return res.end();
+  
+  
 
 }
 
